8ad7fcfced3ef8ac530d1ee7e5ae756c6cb9ebd3
tngo1605 <tuanmanh1605@gmail.com>
2024-12-05 16:13:55 +0700

    them Tuan

--- src/DAO/KhachHang_Dao.java
+++ src/DAO/KhachHang_Dao.java
@@ -2,11 +2,13 @@ package DAO;
 
 public class KhachHang_Dao {
 	private String Khanh; 
-	
+	private String Tuan; 
 	
 	public String in() {
 		return Khanh; 
 		
 	}
-	
+	public String Tuan(){
+            return Tuan;
+        }
 }
